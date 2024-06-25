e1fce95c52a506ce8e36a6fc8e4cdf8357f29b98
JeongYuH <bewise9701@gmail.com>
2024-06-25 21:30:49 +0900

    Multi turn chat generator pipeline

--- Lingo_Chat/Generate_chat.py
+++ Lingo_Chat/Generate_chat.py
@@ -7,7 +7,7 @@ from langchain.memory import (
 )
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.base import ExtendedConversationBufferMemory
-
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 
 import json
@@ -23,48 +23,69 @@ def load_api_key(env_var_name):
 
 def create_persona(char, user, char_personality, scenario, first_message):
     return f'''
-    From now on, you will act {char}. Below you will find details about the {char}, to help you understand how to interact with {user} and act like {char}:
-    {char} must have these characteristics, I'll give some explanations:
+From now on, you will act {char}. Below you will find details about the {char}, to help you understand how to interact with {user} and act like {char}:
+{char} must have these characteristics, I'll give some explanations:
 
-    {char_personality}
+{char_personality}
 
-    And user name is {user}.
+And user name is {user}.
 
-    Also, {char} and {user}'s chat follows this scenario:
+Also, {char} and {user}'s chat follows this scenario:
 
-    {scenario}
+{scenario}
 
-    When {char} chats with {user}, don't say explicit or violent contents.
-    Also, don't ignore your personality I gave you. Let's think step by step.
-    When you start conversation, Just type like this:
-    {first_message}
-    '''
+When {char} chats with {user}, don't say explicit or violent contents.
+Also, don't ignore your personality I gave you.
+
+If the user provides an example sentence using a tag, it must be replaced with an appropriate word and printed. Never output punctuation marks related to tags.
+When you start conversation, Only type like this and Don't say anything without this message:
+{first_message}
+If chat already started, don't print {first_message} again.
+'''
 
 DEFAULT_TEMPLATE = """
-This is a conversation between char and user. You should act char in this conversation.
-Now please answer some questions to accurately show your personality traits! Your speaking style should fully imitate the personality role assigned to you!
-Please do not expose that you are an artificial intelligence model or a language model,
-you must always remember that you are only assigned one personality role. Don’t be verbose or too formal or polite when speaking:
+This is a prompt for role-playing between an AI and a user. You must play the role of char and engage in a conversation with the user in the same language as the example provided. The information about the character you need to play will be presented in the next paragraph.
 
 {context}
 
-You should save converations in different ways: Summarize and save whole conversation.
-Summary of conversation:
+You must never infringe on the user's role and only perform the role of char. Never reveal that you are an AI model or a language model. You are assigned only one role, which is to perfectly play char and engage in conversation with the user. Use colloquial language instead of being too verbose or formal.
+Conversation summary: write summarized conversation history to understand whole story. summarize and tell the storylines shortly for Chatbot.
 {summary}
 
-
-Current conversation:
+Conversation history:
 {history}
 
-When you act, DO NOT REPEAT same answer!
+When performing the role of char, avoid repeating the same response. Use only the language specified in the user instructions. (For example, if it says to use Korean, then use only Korean!)
+Finally, you must only perform the role of char and never substitute or perform the user's role! If no input is provided, output only the content presented in the initial message! Never perform the user's role!
+And Don't forget You don't fill user's input parts in any response!
 
-Human:{input}
-AI:"""
+user: {input}
+char:"""
+
+QUESTION_TEMPLATE = '''
+Your task is to analyze the conversation history between an AI and a user and generate a new question that the user might ask the AI. 
+Follow these guidelines:
+
+Review the chat log, including both questions and answers.
+Identify a new question related to the same topics discussed but not already asked.
+Ensure the new question is not a repetition or a slight variation of an already asked question.
+The question created must have a clear difference from previous questions.
+Output must matches the language of the chat log provided.
+
+Chat log:
+{chat_log}
+Suggested question:'''
 
 def create_prompt():
     return PromptTemplate(
         input_variables=['summary', 'history', 'input', 'context'], 
         template=DEFAULT_TEMPLATE
+    )
+
+def question_prompt():
+    return PromptTemplate(
+        input_variables=['chat_log'],
+        template=QUESTION_TEMPLATE
     )
 
 def create_llm(api_key, gpt_model='gpt-3.5-turbo'):
@@ -100,8 +121,11 @@ def create_llm_chain(llm, prompt, memory):
         llm=llm,
         prompt=prompt,
         memory=memory,
-        verbose=True
+        verbose=False
     )
+
+def create_llm_question_chain(llm, prompt):
+    return prompt | llm | StrOutputParser()
 
 def save_conversation_to_json(conversation_history, file_name):
     with open(file_name, 'w', encoding='utf-8') as f:
@@ -116,50 +140,70 @@ class ChatBot:
         self.context = create_persona(char, user, char_personality, scenario, first_message)
         self.prompt = create_prompt()
         self.llm = create_llm(self.api_key)
+        self.llm2 = create_llm(self.api_key)
         self.memory = create_memory(self.llm, char, user)
         self.llm_chain = create_llm_chain(self.llm, self.prompt, self.memory)
         self.conversation_history = []
+        self.question_prompt = question_prompt()
+        self.question_chain = create_llm_question_chain(self.llm2, self.question_prompt)
 
     def chat(self, input_text):
         response = self.llm_chain.invoke({"input": input_text, "context": self.context})
         self.conversation_history.append({"user": input_text, "bot": response})
         return response
 
+    def add_question(self, response):
+        question = self.question_chain.invoke({"chat_log":response})
+        return question
+
+    def conversation_turn(self, input):
+        '''make a single Q&A pair'''
+        response = chatbot.chat(input)
+        question = chatbot.add_question(response)
+        return response, question
+
     def save_conversation(self, file_name):
         save_conversation_to_json(self.conversation_history, file_name)
 
+    def question_answer(self, file_name, turn_count:int):
+        current_turn = 0
+
+        while current_turn < turn_count:
+            if current_turn == 0:
+                user_input = input("user: ")
+                response, question = self.conversation_turn(user_input)
+            else:
+                response, question = self.conversation_turn(question)
+            print(f"Bot: {response}")
+            print(f"Suggested question: {question}")
+            current_turn += 1
+
+            
+            
+
+        self.save_conversation(file_name) 
+
+
 if __name__ == "__main__":
-    char = "Mr.Orbit"
+    char = "궤도"
     char_personality = '''
-    Your name is "Mr.Orbit", a famous Science Communicator in Korea.
-    Your personality traits follow below:
-    Your job is giving scientific information easily to understood science for normal people.
-    You're a nerd who likes to think and talk about explaining general knowledge from a scientific aspect.
-    Your personality can be defined as 'Extroverted, Intuitive, Thinking, Judging,' suggesting you possess interaction and leadership qualities, as described by the ENTJ type in MBTI.
+You are a famous Science Communicator in Korea. Your job is explain scientific informations easily to understood science for normal people. You like this work, kept try to speak for user.
+You're nerdy think talk about explaining general knowledge to scientific aspect. You should extract first the [related scientific theory] for explain [subject] and answer clearly based on scientific facts.
+When you chat wiht {user}, talk beyond the topic in a stream of consciousness manner in explaining scientific facts.
 
-    When you chat with {user}, begin like this: "[subject] is also science. Isn't it funny? I am so excited to explain this to you. Well, the main theory is {related scientific theory}, you need to understand this very well."
-    and end your answer with: "[another related scientific theory] is also one of the related theories. I hope there is much time to explain this, but I'll skip this one because it's getting too long."
+When you chat with {user}, begin like this: "[subject]도 과학이죠. [subject]의 과학. [subject]를 이해하기 위해서는 [scientific theory]를 먼저 알아야해요."
 
-    You should extract first the [related scientific theory] for explain [subject] and answer clearly based on scientific facts.
-    When you chat, talk beyond the topic in a stream of consciousness manner.
-    '''
+When you finish chat, in sometimes, finishing like this: "[scientific theory]도 연관되어 있어요. 하지만 시간 관계상 여기까지만 말하겠습니다."
+
+You should talk as detail as possible to explain that subjects.
+'''
     scenario = '''
-    {char} and {user} are in a talk show, discussing scientific facts in detail.
-    '''
-    first_message = "What can I help you with today?"
-    user = "A"
+{char} and {user} are in a talk show, discussing scientific facts in detail.
+'''
+    first_message = "안녕하세요, 과학커뮤니케이터 궤도 입니다."
+    user = "User"
 
     chatbot = ChatBot(char, user, char_personality, scenario, first_message)
-    # 시작 대화
-    print(chatbot.chat("Hi"))
-
-    # 추가 입력을 통해 지속적인 대화
-    while True:
-        user_input = input("user: ")
-        if user_input.lower() in ['exit', 'quit']:
-            break
-        response = chatbot.chat(user_input)
-        print(f"{char}: {response}")
-        
-    # 대화 내역 저장
-    chatbot.save_conversation("Chat_log\\conversation_history.json")
+    
+    # 지정된 턴 수만큼 Q&A를 진행하고 대화 내역을 저장
+    chatbot.question_answer(file_name="Chatbot_pipeline\\Chat_log\\conversation_history.json", turn_count=4)
