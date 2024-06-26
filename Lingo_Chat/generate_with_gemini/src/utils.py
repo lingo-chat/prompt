28760c8b1a6636c5a80e9bb60d1d37abf0981fb4
Lewis JooHyeong Lee <jhhome3190@naver.com>
2024-06-26 22:27:46 +0900

    Feat: post processing - - 싱글턴, 멀티턴 데이터에 대한 간단한 후처리(regex) 함수 적용 - datasetdict 저장, hf 업로드 함수 적용

--- Lingo_Chat/generate_with_gemini/src/utils.py
+++ Lingo_Chat/generate_with_gemini/src/utils.py
@@ -1,5 +1,9 @@
+import os
 import json
+import jsonlines
 import aiohttp
+
+from datasets import Dataset, DatasetDict
 
 
 SAFETY_SETTING = [
@@ -9,6 +13,7 @@ SAFETY_SETTING = [
     {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"},
 ]
 
+
 def jsonl_save(dir, json_data):
     with open(dir, "a+", encoding="utf-8") as f:
         json.dump(
@@ -17,7 +22,18 @@ def jsonl_save(dir, json_data):
         f.write(
             "\n"
         )  # json을 쓰는 것과 같지만, 여러 줄을 써주는 것이므로 "\n"을 붙여준다.
-        
+
+
+def jsonl_read(file_dir: str):
+    """
+    jsonl 파일을 읽어서 list로 반환합니다.
+    """
+    result = []
+    with jsonlines.open(file_dir) as f:
+        for _data in f.iter():
+            result.append(_data)
+    return result
+
 
 async def llm_query(
     model_url: str, api_key: str, _input: list, _generation_config: dict
@@ -66,3 +82,109 @@ async def stream_response(url, headers, data):
                         print(chunk, end="", flush=True)
                 except Exception as e:
                     pass
+
+
+def upload_to_hf(
+    _datasetdict,
+    directory,
+    save_file_name,
+    save_directory=None,
+    hf_repo_name=None,
+    hf_commit_message=None,
+):
+    """
+    Upload to HF
+    """
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+    HF_TOKEN = os.getenv("HF_TOKEN")
+
+    # parquet_dir = "parquet"
+    # parquet_load_dir = os.path.join(directory, parquet_dir)
+
+    # if not os.path.isdir(parquet_load_dir):
+    #     os.makedirs(parquet_load_dir)
+
+    print(
+        f"\n\n>> [DatasetDict] ({directory})\n[Num of rows are]: {len(_datasetdict['train'])}\n[Parquet name]: {save_directory}\n\n"
+    )
+    if hf_commit_message is None:
+        hf_commit_message = f"Upload Dataset - {save_file_name}"
+
+    try:
+        train_dataset = _datasetdict
+        train_dataset.push_to_hub(
+            hf_repo_name,
+            token=HF_TOKEN,
+            config_name=save_file_name,
+            commit_message=hf_commit_message,
+            private=False,
+        )
+        print(f">> Upload to HF is done !\n\n")
+
+    except Exception as E:
+        print(f"ERROR with saving {directory}, {E}")
+
+
+def save_into_datadict(
+    train_file_dir: str = "",
+    test_file_dir: str = "",
+    save_directory: str = "",
+    save_file_name: str = "",
+    hf_repo_name: str = "",
+    hf_commit_message: str = "",
+):
+    """
+    jsonl포맷 파일을 Datadict 형태로 변환하여 저장합니다.
+    """
+
+    def _gen(_list):
+        for item in _list:
+            yield item
+
+    train_list = jsonl_read(train_file_dir)
+    test_list = None
+    if test_file_dir != "" and test_file_dir is not None:
+        test_list = jsonl_read(test_file_dir)
+
+    save_directory = "../data/datasets"
+    save_file_name = save_file_name
+    save_directory = os.path.join(save_directory, save_file_name)
+
+    if not os.path.exists(save_directory):
+        os.makedirs(save_directory)
+
+    # datasetdict 으로 변환
+    train_converted = Dataset.from_generator(lambda: _gen(train_list))
+    if test_list:
+        test_converted = Dataset.from_generator(lambda: _gen(test_list))
+        _datasetdict = text_converted = DatasetDict(
+            {"train": train_converted, "test": test_converted}
+        )
+    else:
+        _datasetdict = text_converted = DatasetDict({"train": train_converted})
+    text_converted.save_to_disk(save_directory)
+
+    print(f">> Now Upload to HF . . .")
+
+    upload_to_hf(
+        _datasetdict=_datasetdict,
+        directory=save_directory,
+        save_file_name=save_file_name,
+        # save_directory=dataset_dir,
+        hf_repo_name=hf_repo_name,
+        hf_commit_message=hf_commit_message,
+    )
+
+
+# if __name__ == "__main__":
+#     save_into_datadict(
+#         train_file_dir="../data/multiturn_data_0625_1_post.jsonl",
+#         train_file_dir=None,
+#         save_directory="../data/datasets",
+#         save_file_name="KoAlpaca_v1.1_orbit_v1.5_multiturn",
+#         hf_repo_name="DinoTheLewis/KoAlpaca_persona_multiturn",
+#         hf_commit_message="Upload Dataset - KoAlpaca_v1.1_orbit_v1.5_multiturn",
+#     )
