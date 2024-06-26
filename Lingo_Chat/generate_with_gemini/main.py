b329b09a390e99835b8f11fb2fc0634a64222707
Lewis JooHyeong Lee <jhhome3190@naver.com>
2024-06-26 17:13:21 +0900

    Feat: multithread, multi api key - multi api key 와 multithread를 이용하여 RPM 향상 - max request 일 경우 pass 되는 데이터 로깅 방법 향상 가능성이 남아있음

--- Lingo_Chat/generate_with_gemini/main.py
+++ Lingo_Chat/generate_with_gemini/main.py
@@ -4,57 +4,62 @@
     
     - reference: Ultrachat
     - dataset: HEYPAL/persona_orbit_dataset
-    - model: GEMINI API
+    - model: GEMINI pro 1.0 free
     - creator: Joo Hyeong Lee
 """
 import os
 import asyncio
+import concurrent.futures
 
+from dotenv import load_dotenv
 from datasets import load_dataset
 from typing import Dict, List
+from tqdm import tqdm
 
-from src.api import gemini, gemini_api_key
+from src.api import gemini
 from src.utils import jsonl_save
 from src.prompt import system_prompt, orbit_role_name, orbit_role_description, question_induce_prompt, answer_induce_prompt
 
-system_prompt = system_prompt.format(role_name=orbit_role_name, role_description_and_catchphrases=orbit_role_description)
 
-
-def main(model_name: str,
+def main(system_prompt:str,
+         model_name: str,
+         gemini_api_key_list: List[str],
          data_save_path: str,
-         target_turn: int = 3,) -> None:
+         target_turn: int = 3,
+         start_idx: int = 0) -> None:
     """
+    Multiturn 데이터를 생성 main code
+    
+    생성 시퀀스:
         1. KoAlpaca_v1.1_orbit_v1.4.3 데이터 불러오기
         2. 첫 번째 turn 로드
         3. (시스템 프롬프트 + 첫 번째 turn + user 질문생성 프롬프트) 모델 전송 -> 생성된 질문 async receive
             ((동일한 프롬프트) + 모델 답변=질문 + assistant 답변생성 프롬프트) 모델 전송 -> 답변 생성
-        4. 원하는만큼 3번 반복 후 종료(총 n 턴 데이터 생성)
+        4. 원하는만큼 반복 후 종료(총 n 턴 데이터 생성)
     """
     
     koalpaca_orbit = load_dataset("HEYPAL/persona_orbit_dataset", "KoAlpaca_v1.1_orbit_v1.4.3")['train']
     
-    for idx, _data in enumerate(koalpaca_orbit):
-        # if idx > 100: break
+    def _process_data(idx, _data):
         print(f"\n\n\n------------------------- {idx} -------------------------\n")
+        gemini_api_key = gemini_api_key_list[idx%len(gemini_api_key_list)]
+        
         now_turn = 1
         
-        first_user = _data['instruction']
-        first_model = _data['output']
-        
         # 첫 번째 turn 생성
-        _num_first_answer = count_answer_length(first_model)
+        _num_first_answer = count_answer_length(_data['output'])
         ans_num_paragraphs = [_num_first_answer[0]]
         ans_num_words = [_num_first_answer[1]]
-        messages = [{'role': 'user', 'content': first_user},
-                    {'role': 'assistant', 'content': first_model},]
+        messages = [{'role': 'user', 'content': _data['instruction']},
+                    {'role': 'assistant', 'content': _data['output']},]
         
-        _input = system_prompt+'\n\nUser: '+first_user+'\nAssitant: '+first_model+'\n\n\n'
+        _input = system_prompt+'\n\nUser: '+_data['instruction']+'\nAssitant: '+_data['output']+'\n\n\n'
         
         # 원하는 turn 수 만큼 생성
         while(now_turn < target_turn):
             try:
                 # 새로운 turn 생성
-                _gen_new_turn = gen_new_turn(model_name, _input, question_induce_prompt, answer_induce_prompt)
+                _gen_new_turn = gen_new_turn(model_name, gemini_api_key, _input, question_induce_prompt, answer_induce_prompt)
                 
                 # 생성된 turn의 길이 계산
                 _num_new_answer = count_answer_length(_gen_new_turn['gen_answer'])
@@ -68,29 +73,36 @@ def main(model_name: str,
                 now_turn += 1
                 _input = _input.split('\n\n\n')[0]+'\nUser: '+_gen_new_turn['gen_question']+'\nAssistant: '+_gen_new_turn['gen_answer']+'\n\n\n'
             
-            
             except Exception as e:
                 print(f"Error: {e}\n")
                 with open(os.path.basename(data_save_path).split('.')[0]+'_error.txt', 'a+') as f:
                     f.write(f"{idx}번째 데이터에서 에러 발생\n")
+                break
+        
+        if len(ans_num_paragraphs) > 1:
+            # 생성된 데이터 포맷 세팅
+            _new_multiturn_data = {
+                'instruction': _data['instruction'],
+                'ans_num_paragraphs': ans_num_paragraphs,
+                'ans_num_words': ans_num_words,
+                'num_turns': len(ans_num_paragraphs),
+                'messages': messages,
+                'output': _data['output'],
+                'org_output': _data['org_output'],
+                'url': _data['url'],
+            }
             
-        # 생성된 데이터 포맷 세팅
-        _new_multiturn_data = {
-            'instruction': _data['instruction'],
-            'ans_num_paragraphs': ans_num_paragraphs,
-            'ans_num_words': ans_num_words,
-            'num_turns': target_turn,
-            'messages': messages,
-            'output': _data['output'],
-            'org_output': _data['org_output'],
-            'url': _data['url'],
-        }
-        
-        # 생성된 데이터 저장
-        jsonl_save(data_save_path, _new_multiturn_data)
+            # 생성된 데이터 저장
+            jsonl_save(data_save_path, _new_multiturn_data)
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers=len(gemini_api_key_list)) as executor:
+        futures = [executor.submit(_process_data, idx, _data) for idx, _data in enumerate(koalpaca_orbit) if idx >= start_idx]
+        for future in tqdm(concurrent.futures.as_completed(futures), total=(len(koalpaca_orbit)-start_idx)):
+            future.result()
 
-        
+
 def gen_new_turn(model_name: str,
+                 gemini_api_key: str,
                  _user_input: str,
                  question_induce_prompt: str, 
                  answer_induce_prompt: str) -> Dict[str, str]:
@@ -107,7 +119,7 @@ def gen_new_turn(model_name: str,
         gen_answer: str - 생성된 답변
     """
     
-    async def _run(_user_input, _induce_prompt, temperature) -> str:
+    async def _run(_user_input, _induce_prompt, gemini_api_key, temperature) -> str:
         chat_history = [
             {"role": "user", 
                 "parts": [{
@@ -129,9 +141,9 @@ def gen_new_turn(model_name: str,
         print(f"gen:\n{response}\n\n")
         return response   
     
-    gen_question = asyncio.run(_run(_user_input, question_induce_prompt, 0.5))
+    gen_question = asyncio.run(_run(_user_input, question_induce_prompt, gemini_api_key, 0.5))
     _new_user_input = _user_input.split('\n\n\n')[0]+'\nUser: '+gen_question+'\n\n\n'
-    gen_answer = asyncio.run(_run(_new_user_input, answer_induce_prompt, 0))
+    gen_answer = asyncio.run(_run(_new_user_input, answer_induce_prompt, gemini_api_key, 0))
     
     return {'gen_question': gen_question, 'gen_answer': gen_answer}
     
@@ -147,6 +159,17 @@ def count_answer_length(answer) -> List[int]:
 
 
 if __name__ == '__main__':
-    main(model_name="geminipro1.0",
-         data_save_path='data/multiturn_data_0625_first50.jsonl',
-         target_turn=3)
+    
+    ### setting - you need to set these first
+    system_prompt = system_prompt.format(role_name=orbit_role_name, role_description_and_catchphrases=orbit_role_description)
+    load_dotenv()
+    gemini_api_key_list = [os.getenv(f"{i}_GEMINI_API_KEY") for i in ["LE"]]    # ["LE", "JH", "JY", "YH"]
+    start_idx = 1374
+
+    main(system_prompt=system_prompt,
+         model_name="geminipro1.0",
+         gemini_api_key_list=gemini_api_key_list,
+         data_save_path='data/multiturn_data_0626_2.jsonl',
+         target_turn=3,
+         start_idx=start_idx
+         )
