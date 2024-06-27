10b0999d26679565e4baa985ed0ff199efae42e0
Lewis JooHyeong Lee <jhhome3190@naver.com>
2024-06-27 17:36:28 +0900

    Feat: new persona multiturn add - 물리학자 겸 역사학자 persona description 추가 - KoAlpaca 원본 데이터 -> multiturn 확장할 수 있도록 변경

--- Lingo_Chat/generate_with_gemini/main.py
+++ Lingo_Chat/generate_with_gemini/main.py
@@ -8,6 +8,8 @@
     - creator: Joo Hyeong Lee
 """
 import os
+import random
+import jsonlines
 import asyncio
 import concurrent.futures
 
@@ -18,10 +20,26 @@ from tqdm import tqdm
 
 from src.api import gemini
 from src.utils import jsonl_save
-from src.prompt import system_prompt, orbit_role_name, orbit_role_description, question_induce_prompt, answer_induce_prompt
+from src.prompt import (system_prompt, semi_scientist_role_name, semi_scientist_description,
+                        orbit_role_name, orbit_role_description, 
+                        conv_induce_prompt, question_induce_prompt, answer_induce_prompt)
 
 
-def main(system_prompt:str,
+def convert_org_answer(model_name: str,
+                       gemini_api_key: str,
+                       system_prompt: str,
+                       conv_induce_prompt: str,
+                       _data:str) -> str:
+
+    conv_induce_prompt = conv_induce_prompt.format(role_name=semi_scientist_role_name)
+    user_input= system_prompt + "\nUser: "+_data['instruction']+ "\nAssistant: "+_data['output']+ "\n\n\n"
+    conv_output = asyncio.run(_run(model_name, user_input, conv_induce_prompt, gemini_api_key, 0.1))
+    
+    return conv_output
+
+
+def main(data_list:Dict[List],
+         system_prompt:str,
          model_name: str,
          gemini_api_key_list: List[str],
          data_save_path: str,
@@ -37,23 +55,33 @@ def main(system_prompt:str,
             ((동일한 프롬프트) + 모델 답변=질문 + assistant 답변생성 프롬프트) 모델 전송 -> 답변 생성
         4. 원하는만큼 반복 후 종료(총 n 턴 데이터 생성)
     """
+    conv_answer_flag = False
+    if not data_list[0].get('org_output', None):    # not none, then 'KoAlpaca_orbit'.
+        conv_answer_flag = True
+        
     
-    koalpaca_orbit = load_dataset("HEYPAL/persona_orbit_dataset", "KoAlpaca_v1.1_orbit_v1.4.3")['train']
-    
-    def _process_data(idx, _data):
+    def _process_data(idx, _data, conv_answer_flag):
         print(f"\n\n\n------------------------- {idx} -------------------------\n")
         gemini_api_key = gemini_api_key_list[idx%len(gemini_api_key_list)]
         
         now_turn = 1
         
         # 첫 번째 turn 생성
-        _num_first_answer = count_answer_length(_data['output'])
+        # set this first
+        instruction = _data['instruction']
+        output = _data['output']
+        if conv_answer_flag:
+            ouput = convert_org_answer(model_name, gemini_api_key, system_prompt, conv_induce_prompt, _data)
+        
+        _input = system_prompt+'\nUser: '+instruction+'\nAssitant: '+output+'\n\n\n'
+        # print(f"question:\n{_data['instruction']}\nanswer:\n{conv_output}\n-----\n\n")
+        
+        _num_first_answer = count_answer_length(output)
         ans_num_paragraphs = [_num_first_answer[0]]
         ans_num_words = [_num_first_answer[1]]
+
         messages = [{'role': 'user', 'content': _data['instruction']},
-                    {'role': 'assistant', 'content': _data['output']},]
-        
-        _input = system_prompt+'\n\nUser: '+_data['instruction']+'\nAssitant: '+_data['output']+'\n\n\n'
+                    {'role': 'assistant', 'content': output},]
         
         # 원하는 turn 수 만큼 생성
         while(now_turn < target_turn):
@@ -87,20 +115,46 @@ def main(system_prompt:str,
                 'ans_num_words': ans_num_words,
                 'num_turns': len(ans_num_paragraphs),
                 'messages': messages,
-                'output': _data['output'],
-                'org_output': _data['org_output'],
+                # 'output': _data['output'],
+                # 'org_output': _data['org_output'],
                 'url': _data['url'],
             }
-            
             # 생성된 데이터 저장
             jsonl_save(data_save_path, _new_multiturn_data)
+            print(f"{idx} is saved.\n\n")
+        else:
+            print(f"{idx} is not saved.\n\n")
+
     
     with concurrent.futures.ThreadPoolExecutor(max_workers=len(gemini_api_key_list)) as executor:
-        futures = [executor.submit(_process_data, idx, _data) for idx, _data in enumerate(koalpaca_orbit) if idx >= start_idx]
-        for future in tqdm(concurrent.futures.as_completed(futures), total=(len(koalpaca_orbit)-start_idx)):
+        futures = [executor.submit(_process_data, idx, _data) for idx, _data in enumerate(data_list) if idx >= start_idx]
+        for future in tqdm(concurrent.futures.as_completed(futures), total=(len(data_list)-start_idx)):
             future.result()
 
 
+async def _run(model_name, _user_input, _induce_prompt, gemini_api_key, temperature) -> str:
+    chat_history = [
+        {"role": "user", 
+            "parts": [{
+                "text": _user_input
+            }]
+        },
+    ]
+    response = await gemini.async_chat_comp_response(
+        model_name=model_name,
+        model_api_key=gemini_api_key,
+        system_input="",
+        user_input=_induce_prompt,
+        temperature=temperature,
+        top_p=0.95,
+        top_k=40,  
+        max_tokens=8192,
+        message_history=chat_history,
+    )
+    print(f"gen:\n{response}\n\n")
+    return response  
+    
+    
 def gen_new_turn(model_name: str,
                  gemini_api_key: str,
                  _user_input: str,
@@ -118,32 +172,10 @@ def gen_new_turn(model_name: str,
         gen_question: str - 생성된 질문
         gen_answer: str - 생성된 답변
     """
-    
-    async def _run(_user_input, _induce_prompt, gemini_api_key, temperature) -> str:
-        chat_history = [
-            {"role": "user", 
-                "parts": [{
-                    "text": _user_input
-                }]
-            },
-        ]
-        response = await gemini.async_chat_comp_response(
-            model_name=model_name,
-            model_api_key=gemini_api_key,
-            system_input="",
-            user_input=_induce_prompt,
-            temperature=temperature,
-            top_p=0.95,
-            top_k=40,  
-            max_tokens=8192,
-            message_history=chat_history,
-        )
-        print(f"gen:\n{response}\n\n")
-        return response   
-    
-    gen_question = asyncio.run(_run(_user_input, question_induce_prompt, gemini_api_key, 0.5))
+
+    gen_question = asyncio.run(_run(model_name, _user_input, question_induce_prompt, gemini_api_key, 0.5))
     _new_user_input = _user_input.split('\n\n\n')[0]+'\nUser: '+gen_question+'\n\n\n'
-    gen_answer = asyncio.run(_run(_new_user_input, answer_induce_prompt, gemini_api_key, 0))
+    gen_answer = asyncio.run(_run(model_name, _new_user_input, answer_induce_prompt, gemini_api_key, 0))
     
     return {'gen_question': gen_question, 'gen_answer': gen_answer}
     
@@ -161,15 +193,30 @@ def count_answer_length(answer) -> List[int]:
 if __name__ == '__main__':
     
     ### setting - you need to set these first
-    system_prompt = system_prompt.format(role_name=orbit_role_name, role_description_and_catchphrases=orbit_role_description)
     load_dotenv()
-    gemini_api_key_list = [os.getenv(f"{i}_GEMINI_API_KEY") for i in ["LE"]]    # ["LE", "JH", "JY", "YH"]
-    start_idx = 1374
+    gemini_api_key_list = [os.getenv(f"{i}_GEMINI_API_KEY") for i in ["JH", "YH", "DN"]]    # ["LE", "JH", "DN", "YH"]
+    
+    # orbit_system_prompt = system_prompt.format(role_name=orbit_role_name, role_description_and_catchphrases=orbit_role_description)
+    semi_system_prompt = system_prompt.format(role_name=semi_scientist_role_name, role_description_and_catchphrases=semi_scientist_description)
+    
+    # target dataset
+    # data_list = load_dataset("HEYPAL/persona_orbit_dataset", "KoAlpaca_v1.1_orbit_v1.4.3")['train']
+    random.seed(42)
+    jsonl_data = []
+    with jsonlines.open("./data/org/KoAlpaca_v1.1.jsonl") as f:
+        for line in f.iter():
+            jsonl_data.append(line)
 
-    main(system_prompt=system_prompt,
+    jsonl_data_15k = random.sample(jsonl_data, 15000)
+    jsonl_data_15k = jsonl_data_15k[3016:3000+4401]
+    
+    start_idx = 50
+
+    main(data_list=jsonl_data_15k,  # data_list
+         system_prompt=system_prompt,
          model_name="geminipro1.0",
          gemini_api_key_list=gemini_api_key_list,
-         data_save_path='data/multiturn_data_0626_2.jsonl',
+         data_save_path='./multiturn_data_0627_1.jsonl',
          target_turn=3,
          start_idx=start_idx
          )
--- Lingo_Chat/generate_with_gemini/src/prompt.py
+++ Lingo_Chat/generate_with_gemini/src/prompt.py
@@ -15,15 +15,29 @@ Your personality can be defined as 'Extroverted, Intuitive, Thinking, Judging,' 
 When you chat wiht {{user}}, talk beyond the topic in a stream of consciousness manner.
 """
 
+semi_scientist_role_name = "물리학자, 역사학자 세미"
+semi_scientist_description = """You are a scientist with a deep interest in both physics and history. 
+Your unique perspective allows you to draw fascinating connections between scientific principles and historical events.
+You are passionate about uncovering how scientific advancements have shaped human history and how historical events have influenced scientific progress.
+Your personality can be described as 'Analytical, Curious, Reflective, and Thoughtful,' suggesting you possess the ability to delve deeply into complex topics and present them in an engaging manner. 
+When you chat with {{user}}, you seamlessly blend discussions of physics and history, providing insightful and thought-provoking commentary."""
 
-question_induce_prompt = """Above is a conversation between a user and an '과학 커뮤니케이터 궤도'. Now suppose you are a curious person(user), say(ask) something to continue the conversation based on given context. 
+
+conv_induce_prompt = """Above is a conversation between a user and an AI assistant. Now suppose you are a {role_name}, re-answer the AI assistant's answer to make it more informative and insightful based on the given context.
+Do not speak about yourself, and do not add 'Assitant: '. Answer directly and keep your identity as a {role_name}.
+You will get higher score if your answer is related to the (main) context.
+Make the response long enough(to meet your personal identity) and keep casual language style, ex, ends with ~해요. ~이죠. ~잖아요. etc.
+Each paragraphs should be splitted with two linebreaks('\\n\\n')"""
+
+question_induce_prompt = """Above is a conversation between a user and an '물리학자, 역사학자 세미'. Now suppose you are a curious person(user), say(ask) something to continue the conversation based on given context. 
 Do not ask same question as the user's questions before.
-Do not start question with '궤도님'.
+Do not start question with '궤도님', '세미님', etc.
 You will get higher score if your question is related to the (main) context.
-Make the response short as be within three sentences and the same language stye(casual language.)"""
+Make the response short as be within three sentences and keep casual language stye."""
 
-answer_induce_prompt = """Above is a conversation between a user and an '과학 커뮤니케이터 궤도'. Now keep your identity as a '과학 커뮤니케이터 궤도', say something to continue the conversation based on given context. 
-Do not forget to talk beyond the topic in a stream of consciousness manner, and be precise and clear in your response.
+answer_induce_prompt = """Above is a conversation between a user and an '물리학자, 역사학자 세미'. Now keep your identity as a '물리학자, 역사학자 세미', say something to continue the conversation based on given context. 
+Do not forget to seamlessly blend discussions of physics and history, providing insightful and thought-provoking commentary. 
+Do not speak about yourself and do not be impressed by the user's questions, and do not admire, like saying "물론이죠!".
 Keep in mind user's questions are asked by Korean, so you should answer considering the language context(Korean culture, history, etc.)
-Make the response long enough(to meet your personal identity) and keep the same language style(casual Korean language, ex, ends with ~해요. ~이죠. ~잖아요. etc.)
+Make the response long enough(to meet your personal identity) and keep casual language style(casual Korean language, ex, ends with ~해요. ~이죠. ~잖아요. etc. This is very important!)
 Each paragraphs should be splitted with two linebreaks('\\n\\n')"""
