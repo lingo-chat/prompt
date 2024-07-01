64cce7bd8ab914606214848ef16d8c199eb949bf
Lewis JooHyeong Lee <jhhome3190@naver.com>
2024-07-01 15:32:05 +0900

    Feat: add new persona prompts - neuroticism persona update - historian_and_physician - humanities scholar - and inducing prompts update

--- Lingo_Chat/generate_with_gemini/main.py
+++ Lingo_Chat/generate_with_gemini/main.py
@@ -8,6 +8,7 @@
     - creator: Joo Hyeong Lee
 """
 import os
+import time
 import random
 import jsonlines
 import asyncio
@@ -19,26 +20,51 @@ from typing import Dict, List
 from tqdm import tqdm
 
 from src.api import gemini
-from src.utils import jsonl_save
-from src.prompt import (system_prompt, semi_scientist_role_name, semi_scientist_description,
-                        orbit_role_name, orbit_role_description, 
-                        conv_induce_prompt, question_induce_prompt, answer_induce_prompt)
+from src.utils import jsonl_save, count_answer_length
+from src.prompt import (system_prompt, semi_science_communicator_role_name, semi_science_communicator_description,
+                        orbit_role_name, orbit_role_description,
+                        neuroticism_role_name, neuroticism_role_description,
+                        humanities_scholar_role_name, humanities_scholar_description,
+                        question_conv_induce_prompt, conv_induce_prompt, 
+                        question_induce_prompt, answer_induce_prompt)
+
+
+def convert_org_question(model_name: str,
+                       gemini_api_key: str,
+                       system_prompt: str,
+                       question_conv_induce_prompt: str,
+                       _data:str) -> str:
+    
+    # question_conv_induce_prompt = question_conv_induce_prompt.format(role_name=neuroticism_role_name)
+    user_input= system_prompt + "\nUser: "+_data['instruction']+"\n\n\n"
+    conv_output = asyncio.run(_run(model_name, user_input, question_conv_induce_prompt, gemini_api_key, 0.4))
+    
+    return conv_output
 
 
 def convert_org_answer(model_name: str,
                        gemini_api_key: str,
                        system_prompt: str,
                        conv_induce_prompt: str,
-                       _data:str) -> str:
+                       instruction: str,
+                       output: str) -> str:
 
-    conv_induce_prompt = conv_induce_prompt.format(role_name=semi_scientist_role_name)
-    user_input= system_prompt + "\nUser: "+_data['instruction']+ "\nAssistant: "+_data['output']+ "\n\n\n"
-    conv_output = asyncio.run(_run(model_name, user_input, conv_induce_prompt, gemini_api_key, 0.1))
+    neuroticism_appendix = ["귀찮게 구시는군요. 크게 될 사람이네요. 귀찮지만 설명해드리죠. \n", # "이런 것도 설명해줘야 해요..? 저는 이 분야 전문가가 아닌데요. 그래도 설명해보죠.\n",
+                            "그만 물어보세요. 궁금한게 왜이렇게 많은거에요? 수준맞춰 설명하려면 하루종일 걸릴 것 같네요. \n\n"
+                            "이런 건 왜 궁금한거죠? 번거롭네요. 설명하자면, \n", # "이런 건 상식 아닌가요? 번거롭네요.\n", 
+                            "질문 수준이 참 재밌네요. 이렇게 질문해서는 성장할 수 없어요. 그래도 수준에 맞춰 설명해드리죠. \n",
+                            "질문 꼬라지 하고는... 설명하기 싫어지는군요.\n", 
+                            "이런 것도 궁금하다니, 세상이 참 재미있겠어요. 저는 재밌는 사람이니까, 설명해드리죠.\n"]
+    
+    conv_induce_prompt = conv_induce_prompt.format(role_name=neuroticism_role_name, neuroticism_appendix=random.choice(neuroticism_appendix))
+    user_input= system_prompt + "\nUser: "+instruction+"\n\n\n"
+    # "\nAssistant: "+output+ "\n\n\n"
+    conv_output = asyncio.run(_run(model_name, user_input, conv_induce_prompt, gemini_api_key, 0.2))
     
     return conv_output
 
 
-def main(data_list:Dict[List],
+def main(data_list:List[Dict],
          system_prompt:str,
          model_name: str,
          gemini_api_key_list: List[str],
@@ -71,8 +97,10 @@ def main(data_list:Dict[List],
         instruction = _data['instruction']
         output = _data['output']
         if conv_answer_flag:
-            ouput = convert_org_answer(model_name, gemini_api_key, system_prompt, conv_induce_prompt, _data)
-        
+            instruction = convert_org_question(model_name, gemini_api_key, system_prompt, question_conv_induce_prompt, _data)
+            output = convert_org_answer(model_name, gemini_api_key, system_prompt, conv_induce_prompt, instruction, _data['output'])
+            # print(f"-----insturction: {instruction}\n\noutput: {output}\n-----\n")
+            
         _input = system_prompt+'\nUser: '+instruction+'\nAssitant: '+output+'\n\n\n'
         # print(f"question:\n{_data['instruction']}\nanswer:\n{conv_output}\n-----\n\n")
         
@@ -80,7 +108,7 @@ def main(data_list:Dict[List],
         ans_num_paragraphs = [_num_first_answer[0]]
         ans_num_words = [_num_first_answer[1]]
 
-        messages = [{'role': 'user', 'content': _data['instruction']},
+        messages = [{'role': 'user', 'content': instruction},
                     {'role': 'assistant', 'content': output},]
         
         # 원하는 turn 수 만큼 생성
@@ -100,17 +128,18 @@ def main(data_list:Dict[List],
                 
                 now_turn += 1
                 _input = _input.split('\n\n\n')[0]+'\nUser: '+_gen_new_turn['gen_question']+'\nAssistant: '+_gen_new_turn['gen_answer']+'\n\n\n'
-            
+                time.sleep(6)
+                
             except Exception as e:
                 print(f"Error: {e}\n")
                 with open(os.path.basename(data_save_path).split('.')[0]+'_error.txt', 'a+') as f:
                     f.write(f"{idx}번째 데이터에서 에러 발생\n")
                 break
         
-        if len(ans_num_paragraphs) > 1:
+        if ans_num_paragraphs:
             # 생성된 데이터 포맷 세팅
             _new_multiturn_data = {
-                'instruction': _data['instruction'],
+                'instruction': instruction,
                 'ans_num_paragraphs': ans_num_paragraphs,
                 'ans_num_words': ans_num_words,
                 'num_turns': len(ans_num_paragraphs),
@@ -127,7 +156,7 @@ def main(data_list:Dict[List],
 
     
     with concurrent.futures.ThreadPoolExecutor(max_workers=len(gemini_api_key_list)) as executor:
-        futures = [executor.submit(_process_data, idx, _data) for idx, _data in enumerate(data_list) if idx >= start_idx]
+        futures = [executor.submit(_process_data, idx, _data, conv_answer_flag) for idx, _data in enumerate(data_list) if idx >= start_idx]
         for future in tqdm(concurrent.futures.as_completed(futures), total=(len(data_list)-start_idx)):
             future.result()
 
@@ -146,8 +175,8 @@ async def _run(model_name, _user_input, _induce_prompt, gemini_api_key, temperat
         system_input="",
         user_input=_induce_prompt,
         temperature=temperature,
-        top_p=0.95,
-        top_k=40,  
+        top_p=0.99,
+        top_k=60,  
         max_tokens=8192,
         message_history=chat_history,
     )
@@ -175,29 +204,24 @@ def gen_new_turn(model_name: str,
 
     gen_question = asyncio.run(_run(model_name, _user_input, question_induce_prompt, gemini_api_key, 0.5))
     _new_user_input = _user_input.split('\n\n\n')[0]+'\nUser: '+gen_question+'\n\n\n'
-    gen_answer = asyncio.run(_run(model_name, _new_user_input, answer_induce_prompt, gemini_api_key, 0))
+    gen_answer = asyncio.run(_run(model_name, _new_user_input, answer_induce_prompt, gemini_api_key, 0.05))
     
     return {'gen_question': gen_question, 'gen_answer': gen_answer}
     
-    
-def count_answer_length(answer) -> List[int]:
-    def _count_paragraphs(answer):
-        return len(answer.split('\n\n'))
-    
-    def _count_words(answer):
-        return len(answer.split(' '))
-    
-    return [_count_paragraphs(answer), _count_words(answer)]
 
 
 if __name__ == '__main__':
     
     ### setting - you need to set these first
     load_dotenv()
-    gemini_api_key_list = [os.getenv(f"{i}_GEMINI_API_KEY") for i in ["JH", "YH", "DN"]]    # ["LE", "JH", "DN", "YH"]
+    gemini_api_key_list = [os.getenv(f"{i}_GEMINI_API_KEY") for i in ["LE", "JH", "DN", "YH"]]    # ["LE", "JH", "DN", "YH"]
     
     # orbit_system_prompt = system_prompt.format(role_name=orbit_role_name, role_description_and_catchphrases=orbit_role_description)
-    semi_system_prompt = system_prompt.format(role_name=semi_scientist_role_name, role_description_and_catchphrases=semi_scientist_description)
+    # semi_system_prompt = system_prompt.format(role_name=semi_scientist_role_name, role_description_and_catchphrases=semi_scientist_description)
+    # system_prompt = system_prompt.format(role_name=humanities_scholar_role_name, role_description_and_catchphrases=humanities_scholar_description)
+    system_prompt = system_prompt.format(role_name=neuroticism_role_name, role_description_and_catchphrases=neuroticism_role_description)
+    
+    print(system_prompt, '\n\n\n')
     
     # target dataset
     # data_list = load_dataset("HEYPAL/persona_orbit_dataset", "KoAlpaca_v1.1_orbit_v1.4.3")['train']
@@ -207,16 +231,17 @@ if __name__ == '__main__':
         for line in f.iter():
             jsonl_data.append(line)
 
-    jsonl_data_15k = random.sample(jsonl_data, 15000)
-    jsonl_data_15k = jsonl_data_15k[3016:3000+4401]
+    jsonl_data_15k = random.sample(jsonl_data, 20000)
+    jsonl_data_15k = jsonl_data_15k[3016:]
     
-    start_idx = 50
-
+    # start_idx = 767
+    start_idx = 1330
+    
     main(data_list=jsonl_data_15k,  # data_list
          system_prompt=system_prompt,
          model_name="geminipro1.0",
          gemini_api_key_list=gemini_api_key_list,
-         data_save_path='./multiturn_data_0627_1.jsonl',
+         data_save_path='./data/multiturn_data_0701_1.jsonl',
          target_turn=3,
          start_idx=start_idx
          )
--- Lingo_Chat/generate_with_gemini/src/utils.py
+++ Lingo_Chat/generate_with_gemini/src/utils.py
@@ -3,7 +3,9 @@ import json
 import jsonlines
 import aiohttp
 
+from tqdm import tqdm
 from datasets import Dataset, DatasetDict
+from typing import Any, Dict, List
 
 
 SAFETY_SETTING = [
@@ -24,7 +26,7 @@ def jsonl_save(dir, json_data):
         )  # json을 쓰는 것과 같지만, 여러 줄을 써주는 것이므로 "\n"을 붙여준다.
 
 
-def jsonl_read(file_dir: str):
+def jsonl_read(file_dir: str) -> List[Dict]:
     """
     jsonl 파일을 읽어서 list로 반환합니다.
     """
@@ -35,6 +37,47 @@ def jsonl_read(file_dir: str):
     return result
 
 
+def json_read() -> List[Dict]:
+    """
+    Jsonl이 json형태로 저장된 형태 + 각 json이 줄바꿈으로 저장되어있을 때 사용
+    """
+    json_objects = []
+
+    with open('./Orbit_generated_data_YH_1.jsonl', 'r', encoding='utf-8') as file:
+        json_str = ''
+        for line in file:
+            if line.strip() == '':  # 빈 줄을 만나면 그때까지 읽은 JSON 객체를 파싱
+                if json_str.strip() != '':  # 빈 문자열이 아니면
+                    try:
+                        json_object = json.loads(json_str)
+                        json_objects.append(json_object)
+                    except json.JSONDecodeError as e:
+                        print(f"Error decoding JSON: {e}")
+                    json_str = ''
+            else:
+                json_str += line
+
+        if json_str.strip() != '':
+            try:
+                json_object = json.loads(json_str)
+                json_objects.append(json_object)
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON: {e}")
+
+    return json_objects
+
+
+def count_answer_length(answer) -> List[int]:
+    def _count_paragraphs(answer):
+        return len(answer.split('\n\n'))
+    
+    def _count_words(answer):
+        return len(answer.split(' '))
+    
+    return [_count_paragraphs(answer), _count_words(answer)]
+
+
+### model ouput utility functions
 async def llm_query(
     model_url: str, api_key: str, _input: list, _generation_config: dict
 ):
@@ -82,7 +125,35 @@ async def stream_response(url, headers, data):
                         print(chunk, end="", flush=True)
                 except Exception as e:
                     pass
+### model ouput utility functions
 
+def _append_jsonl(file_path: List[str], 
+                  save_path: str):
+    """
+    흩어진 Jsonl files를 하나로 합치는 함수입니다.
+
+    Args:
+        file_path (List[str]): 파일 경로 리스트
+        save_path (str): 저장경로
+    """
+    total_data = [] 
+    for idx, data_path in tqdm(enumerate(file_path), total=len(file_path), desc='Appending Jsonl files'):
+        _data = jsonl_read(data_path)
+        
+        if '0626' in data_path:
+            _data = list(map(lambda d: {**d, 'persona': 'science_communicator'}, _data))
+        elif 'YH' in data_path:
+            _data = list(map(lambda d: {**d, 'persona': 'semi-science_communicator'}, _data))
+        elif 'history_physics' in data_path:
+            _data = list(map(lambda d: {**d, 'persona': 'historian_and_physicist'}, _data))
+        elif 'humanities' in data_path:
+            _data = list(map(lambda d: {**d, 'persona': 'humanities_scholar'}, _data))
+
+        total_data.extend(_data)
+    
+    for _data in total_data:
+        jsonl_save(save_path, _data)
+        
 
 def upload_to_hf(
     _datasetdict,
@@ -179,12 +250,210 @@ def save_into_datadict(
     )
 
 
-# if __name__ == "__main__":
-#     save_into_datadict(
-#         train_file_dir="../data/multiturn_data_0625_1_post.jsonl",
-#         train_file_dir=None,
-#         save_directory="../data/datasets",
-#         save_file_name="KoAlpaca_v1.1_orbit_v1.5_multiturn",
-#         hf_repo_name="DinoTheLewis/KoAlpaca_persona_multiturn",
-#         hf_commit_message="Upload Dataset - KoAlpaca_v1.1_orbit_v1.5_multiturn",
-#     )
+def _conv_to_multi_chat(example: Dict[str, Any]) -> Dict[str, Any]:
+    """
+        load_dataset으로 불러들인 데이터세트의 example을 multi-turn chat 형식으로 변환합니다.
+        
+        사용:
+            dataset = load_dataset('dataset_name')
+            dataset = dataset.map(_conv_to_multi_chat, num_proc=64)
+        
+        참고:
+            *DatasetDict는 dict 을 반환해야한다.
+        
+        Returns:
+            Dict
+    """
+    if example.get('answer', False):    # role_specific 데이터세트일 경우
+        example['messages'] = [{'role': 'user', 'content': example['question']}, {'role': 'assistant', 'content': example['answer']}]
+    elif example.get('output', False):   # neuroticism 데이터세트일 경우
+        example['messages'] = [{'role': 'user', 'content': example['instruction']}, {'role': 'assistant', 'content': example['output']}]
+    else:                               # koalpaca 데이터세트일 경우
+        example['messages'] = [{'role': 'user', 'content': example['question']}, {'role': 'assistant', 'content': example['chosen']}]
+    return {'messages': example['messages']}
+
+
+def add_system_prompt() -> None:
+    # system prompt 추가해보기
+    """
+    1. orbit persona + orbit specific prompt
+    2. neuroticism
+    3. doctor
+    4. semi-scientist persona / history_physics / humanities
+    5. general persona
+    """
+    from datasets import load_dataset
+
+    from prompt import (system_prompt, system_prompt_without_name,
+                        orbit_role_name, orbit_role_description,
+                        medician_role_name, medician_role_description, 
+                        neuroticism_role_name, neuroticism_role_description,
+                        semi_science_communicator_role_name, semi_science_communicator_description,
+                        historian_and_physician_role_name, historian_and_physician_description,
+                        humanities_scholar_role_name, humanities_scholar_description)
+    
+    orbit_system_prompt = system_prompt.format(role_name=orbit_role_name, role_description_and_catchphrases=orbit_role_description)
+    neuroticism_system_prompt = system_prompt.format(role_name=neuroticism_role_name, role_description_and_catchphrases=neuroticism_role_description)
+    medician_system_prompt = system_prompt.format(role_name=medician_role_name, role_description_and_catchphrases=medician_role_description)
+    # semi_science_communicator_system_prompt = system_prompt.format(role_name=semi_science_communicator_role_name, role_description_and_catchphrases=semi_science_communicator_description)
+    historian_and_physicist_system_prompt = system_prompt.format(role_name=historian_and_physician_role_name, role_description_and_catchphrases=historian_and_physician_description)
+    humanities_scholar_system_prompt = system_prompt.format(role_name=humanities_scholar_role_name, role_description_and_catchphrases=humanities_scholar_description)
+    
+    def _add_system_prompt(data_path: str=None, 
+                           save_path: str=None,
+                           hf_path: str=None,
+                           hf_name: str=None,
+                           system_prompt: str=None,
+                           persona_desc: str=None
+                           ) -> None:
+        """
+        Jsonl data를 읽거나 DatasetDict 형태로 받아서, system prompt를 추가한 뒤, 저장합니다.
+        Args:
+            data_path (str): _description_
+            save_path (str): _description_
+        """
+        def _remove_reversed(example):
+            if not example["reversed"]: return example
+            else:   return None
+
+        def _remove_under_6_turns(example):  # medician 데이터 전용 필터링 함수
+            return int(example["num_turns"]) >= 6  # 3038 rows 중 1366 rows 만 리턴(10 turns)
+
+        def _remove_under_4_score(example):  # KoAlpaca 데이터 전용 필터링 함수
+            return int(example["score"]) >= 4
+        
+        if data_path is not None:
+            data = jsonl_read(data_path)
+            
+            
+        elif hf_path is not None:
+            data = load_dataset(hf_path, hf_name, split='train')
+            if 'persona_data' in hf_name:
+                data = data.filter(_remove_reversed)
+            # if 'medician_persona' in hf_name:
+            #     data = data.filter(_remove_under_6_turns)
+            if 'KoAlpaca_v1.1' in hf_name:
+                data = data.filter(_remove_under_4_score)
+            
+            if not data.to_dict().get('messages', None):
+                data = data.map(_conv_to_multi_chat, num_proc=8)
+                
+            data_dict = data.to_dict()
+            data = [dict(zip(data_dict.keys(), values)) for values in zip(*data_dict.values())]
+        else:
+            raise ValueError("You must provide either data_path or hf_path.")
+        
+        # if system_prompt is None:
+        #     raise ValueError("You must provide system_prompt.")
+        
+        def _attach_system_prompt(example: Dict[str, Any], 
+                                  system_prompt: str,
+                                  persona_desc: str=None) -> Dict[str, Any]:
+            if system_prompt is None:   # 이 경우 general persona 데이터로 간주 -> 페르소나 description이 column에 있는 경우로 간주.
+                system_prompt = system_prompt_without_name.format(role_description_and_catchphrases=example['persona_desc'])
+            example['messages'].insert(0, {"role": "system", "content": system_prompt})
+            
+            if not example.get('persona', None) and persona_desc:
+                example['persona'] = persona_desc
+            
+            # return example
+            return {"messages": example['messages'], "persona": example.get('persona', None)}
+            
+        
+        data_with_system_prompt = [_attach_system_prompt(example, system_prompt, persona_desc) for example in tqdm(data, total=len(data))]
+        if data_path:
+            print(f"\nData path: {os.path.basename(data_path)}\nData length: {len(data)}\n-----\n")
+        if hf_path:
+            print(f"\nData path: {hf_path}/{hf_name}\nData length: {len(data)}\n------\n")
+        for _data in data_with_system_prompt:
+            jsonl_save(save_path, _data)
+        
+        return None
+
+    data_and_sys = [
+        {"data_path": "../data/post/multiturn_data_orbit_multi_0626_1_post.jsonl", "system_prompt": orbit_system_prompt, "persona_desc": "science_communicator"},
+        # {"data_path": "../data/post/Orbit_generated_data_YH_post.jsonl", "system_prompt": semi_science_communicator_system_prompt, "persona_desc": "semi-science_communicator"},
+        {"data_path": "../data/post/multiturn_data_history_physics_post.jsonl", "system_prompt": historian_and_physicist_system_prompt, "persona_desc": "historian_and_physicist"},
+        {"data_path": "../data/post/multiturn_data_humanities_post.jsonl", "system_prompt": humanities_scholar_system_prompt, "persona_desc": "humanities_scholar"},
+    ]
+    data_and_sys_hf = [
+        {"hf_path": "HEYPAL/persona_general_dataset", "hf_name": "044.persona_data", "system_prompt": None, "persona_desc": "general_persona"},
+        {"hf_path": "HEYPAL/persona_general_dataset", "hf_name": "medician_persona", "system_prompt": medician_system_prompt, "persona_desc": "medician"},
+        {"hf_path": "HEYPAL/persona_general_dataset", "hf_name": "neuroticism_persona", "system_prompt": neuroticism_system_prompt, "persona_desc": "neuroticism"},
+        # {"hf_path": "HEYPAL/persona_orbit_dataset", "hf_name": "KoAlpaca_v1.1_orbit_v1.4.3", "system_prompt": orbit_system_prompt, "persona_desc": "science_communicator"},
+    ]
+
+    for _data in data_and_sys:
+        _data_path = _data.get('data_path', None)
+        _system_prompt = _data.get('system_prompt', None)
+        persona_desc = _data.get('persona_desc', None)
+        
+        if _data_path:
+            _add_system_prompt(data_path=_data_path,
+                               save_path="../data/persona_total_sys_0701_1.jsonl",
+                               system_prompt=_system_prompt,
+                               persona_desc=persona_desc)
+    
+    
+    for _data in data_and_sys_hf:
+        hf_path = _data.get('hf_path', None)
+        hf_name = _data.get('hf_name', None)
+        _system_prompt = _data.get('system_prompt', None)
+        persona_desc = _data.get('persona_desc', None)
+        
+        if hf_path and hf_name:
+            _add_system_prompt(# data_path=_data,
+                               hf_path=hf_path,
+                               hf_name=hf_name,
+                               save_path="../data/persona_total_sys_0701_1.jsonl",
+                               system_prompt=_system_prompt,
+                               persona_desc=persona_desc)
+    
+    
+
+if __name__ == "__main__":
+    # system prompt 추가하기
+    from datasets import disable_caching
+    disable_caching()
+    add_system_prompt()
+    
+    
+    # jsonl data를 허깅페이스에 업로드하기
+    save_into_datadict(
+        train_file_dir="../data/persona_total_sys_0701_1.jsonl",
+        test_file_dir=None,
+        save_directory="../data/datasets",
+        save_file_name="Ko_persona_multiturn_v1.2-sys",
+        hf_repo_name="DinoTheLewis/Ko_persona_multiturn_v1.2-sys",
+        hf_commit_message="Upload Dataset - Ko_persona_multiturn_v1.2-sys",
+    )
+    
+    # # 여러 jsonl 파일 합치기
+    # file_path = ["../data/post/multiturn_data_orbit_multi_0626_1_post.jsonl",
+    #              "../data/post/Orbit_generated_data_YH_post.jsonl",
+    #              "../data/post/multiturn_data_history_physics_post.jsonl", 
+    #              "../data/post/multiturn_data_humanities_post.jsonl",]
+    # _append_jsonl(file_path=file_path, 
+    #               save_path="../data/persona_total_0628_1.jsonl")
+
+    
+    # # YH data에 대한 답변 길이 추가
+    # yh_data = jsonl_read("../data/post/Orbit_generated_data_YH_post.jsonl")
+    
+    # for idx, data in tqdm(enumerate(yh_data), total=len(yh_data)):
+    #     ans_num_paragraphs = []
+    #     ans_num_words = []
+
+    #     for _turn in data['messages']:
+    #         if _turn.get('role', None) == 'assistant':
+    #             output = _turn['content']
+    #             _num_first_answer = count_answer_length(output)
+    #             ans_num_paragraphs.append(_num_first_answer[0])
+    #             ans_num_words.append(_num_first_answer[1])
+        
+    #     data['ans_num_paragraphs'] = ans_num_paragraphs
+    #     data['ans_num_words'] = ans_num_words
+    #     data['url'] = None
+    
+    # for _data in yh_data:
+    #     jsonl_save("../data/post/Orbit_generated_data_YH_post_1.jsonl", _data)
