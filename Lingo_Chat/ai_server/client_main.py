52ba20d414382ebf175b2425de70e53859e02131
Lewis JooHyeong Lee <jhhome3190@naver.com>
2024-08-15 01:40:06 +0900

    Feat: socket-io connection w/ api server #19 - socket-io를 활용하여 api server와 연결 함수 작성 - 기존 langgraph 추론 결과를 socket-io로 전송할 수 있도록 변경 - 비동기 수행에 맞게 async redis로 수정

--- Lingo_Chat/ai_server/client_main.py
+++ Lingo_Chat/ai_server/client_main.py
@@ -8,40 +8,64 @@
     3. chatbot을 호출하고 다시 redis에 저장하며, 결과를 리턴함.
 """
 import os
-import redis
+import socketio
+import redis.asyncio as redis
 import asyncio
+# import websockets
 
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage, AIMessage
-
 from graph import chat_graph, config
 
-### Redis 설정
+##########
+### dotenv setting
+##########
 load_dotenv(override=True)
-redis_url = os.getenv('REDIS_URL')
+redis_url = os.getenv('VM_URL')
 redis_port = os.getenv('REDIS_PORT')
-redis_ms_id = os.getenv('REDIS_MS_ID')
+redis_ms_id = os.getenv('REDIS_MS_ID')   # user_ms_queue
+
+websocket_url = "http://"+os.getenv('VM_URL')+":"+os.getenv('API_PORT')
+websocket_namespace = os.getenv('API_WS_NAMESPACE')
+websocket_event = os.getenv('API_WS_EVENTNAME')
+
+connection_test_message = "ai 서버에서 보내는 연결 테스트 메세지 입니다."
+
+asio = socketio.AsyncClient(logger=True, engineio_logger=True)
 
 
-async def hello():
-    uri = redis_url
-    r = redis.Redis(host=uri, port=redis_port, db=0)
+##########
+### socket-io event handler
+##########
+@asio.on(event='connect', namespace=websocket_namespace)
+async def connect_handler():
+    print(f"\n>> api 서버와 웹소켓 연결 성공!\nsio id: {asio.sid}, transport: {asio.transport}\n\n")
+    await asio.emit('ai_chat_message', connection_test_message, namespace=websocket_namespace)
     
+    
+async def process_message(ws_server, event_name, namespace):
+    """
+        1. redis 에서 user_message, chat_room_id를 받아옴
+        2. graph를 통해 inference 진행 후 ws으로 전송
+        3. redis에 chat_history 저장
+    """
     while True:
-        _, message = r.brpop(redis_ms_id)
+        uri = redis_url
+        r = redis.Redis(host=uri, port=redis_port, db=1)
+        
+        _, message = await r.brpop(redis_ms_id)
         
         # 1. 메세지 수신
         message = eval(message.decode('utf-8'))
         user_id, user_message = message['chat_room_id'], message['user_message']
         print(f"chat_room_id: {user_id}, message: {user_message}\n\n")
         
-        
         # 2. 히스토리 조회 -> AIMessages, HumanMessages 로 변환이 필요
-        def _get_chat_history(user_id):
+        async def _get_chat_history(user_id):
             """
                 _chat_history == [bytes(list(dict))]
             """
-            _chat_history = r.lrange(user_id, 0, -1)
+            _chat_history = await r.lrange(user_id, 0, -1)
             chat_history = []
             
             # 3. chat format으로 다시 변경
@@ -56,8 +80,8 @@ async def hello():
             # chat_history = [eval(data.decode('utf-8')) for data in _chat_history]
             return chat_history
         
-        chat_history = _get_chat_history(user_id)
-        # print(f"\n\n>> chat_history: {chat_history}\n\n")
+        chat_history = await _get_chat_history(user_id)
+        print(f"\n\n>> chat_history: {chat_history}\n\n")
 
         # 4. chatbot 호출
         response = chat_graph.astream_events({"history": chat_history, "messages": user_message}, config=config, version='v1')    # return: async_generator
@@ -67,22 +91,38 @@ async def hello():
                 chatbot_messages = resp['data']['chunk'].content
                 if chatbot_messages and resp['name'] == 'ChatOpenAI':
                     # for i in chatbot_messages:
-                    print(chatbot_messages, end="", flush=True)
+                    # print(chatbot_messages, end="", flush=True)
+                    # ws_server.send(chatbot_messages)
+                    await ws_server.emit(event_name, chatbot_messages, namespace=namespace)
                     final_response += chatbot_messages
                     
                 if resp['name'] == 'ChatOpenAI' and resp['data']['chunk'].response_metadata['finish_reason'].lower() == 'stop':
+                    print(f"\n>> [Langgraph 생성 답변]: {final_response}\n\n")
                     print(f"\n>> [response handler] finished...\n\n")
                     
             except:
                 pass
-            # if response == '<|im_end|>':
-            #     break
- 
+        
         # 5. redis history 저장
         save_messages = [
             str({'role': 'user', 'content': user_message}),
             str({'role': 'assistant', 'content': final_response})
         ]
-        r.rpush(user_id, *save_messages)
-
-asyncio.get_event_loop().run_until_complete(hello())
+        await r.rpush(user_id, *save_messages)
+    
+ 
+async def main():
+    try:
+        # socket io 연결
+        await asio.connect(websocket_url, namespaces=[websocket_namespace])
+        await asio.sleep(seconds=1)
+        
+        # 메인 추론 함수 비동기 실행
+        asyncio.create_task(process_message(ws_server=asio, event_name='ai_chat_message', namespace=websocket_namespace))
+        await asio.wait()
+            
+    except Exception as e:
+        print(f"\n>> api 서버와 웹소켓 연결에 실패했습니다.\n>> 에러 메세지: {e}\n")        
+        await asio.disconnect()
+        
+asyncio.get_event_loop().run_until_complete(main())
