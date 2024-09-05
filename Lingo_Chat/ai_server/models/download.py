988cf06b4a631042398a581104b729932d259811
Lewis JooHyeong Lee <jhhome3190@naver.com>
2024-09-05 12:41:29 +0900

    Feat: model download codea add

---
+++ Lingo_Chat/ai_server/models/download.py
@@ -0,0 +1,26 @@
+### download model
+import torch
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+
+def _model_download(
+    model_path: str = "HEYPAL/PAL_orbit_v0.2.2.3",
+    save_path: str = "./PAL_orbit_v0.2.2.3",
+):
+    """
+    모델 다운로드
+    """
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.bfloat16)
+    try:
+        model.save_pretrained(save_path)
+        tokenizer.save_pretrained(save_path)
+
+        print(f"Model Downloaded Successfully!!!\n\n")
+    except Exception as E:
+        print(f"Error occured in Model Downloaded!!\nError: {E}\n\n")
+
+
+if __name__ == "__main__":   
+    _model_download()
